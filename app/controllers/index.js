b1d9b6a40a71a8d75dc0859e647d4f4ca47ae800
Santiago <santiago@santiago-VirtualBox.(none)>
2014-11-22 21:37:18 +0100

    Cambios iniciales

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,21 +3,28 @@ $.index.open();
 Alloy.Collections.Farmacos.fetch();
 
 var peso=10.0;
+var dosis=2.0;
 
 function cleanup() {
     $.destroy();
 }
 
 function farmaco(event) {
-	Ti.API.info(event.selectedValue);
+	Ti.API.info("Farmaco elegido: "+ event.selectedValue);
 	var selectedFarmaco = Alloy.Collections.Farmacos.where({Farmaco: event.row.title})[0];
-	Ti.API.info(selectedFarmaco.get('NombreComercial'));
-	var rangoTerapeutico = "Rango terapeútico en μg/Kg/min: "+selectedFarmaco.get('RangoTerapMin') + "-" + selectedFarmaco.get('RangoTerapMax');
+	Ti.API.info("Nombre Comercial: " + selectedFarmaco.get('NombreComercial'));
+	var rangoTerapeutico = selectedFarmaco.get('RangoTerapMin') + "-" + selectedFarmaco.get('RangoTerapMax');
 	$.RangoTerap.setText(rangoTerapeutico);
+	$.Dosis.setValue(selectedFarmaco.get('RangoTerapMin'));
 }
 
 
 function peso(event) {
 	peso=$.Peso.getValue();
-	Ti.API.info(peso);
+	Ti.API.info("peso: " + peso);
+}
+
+function dosis(event) {
+	dosis=$.Dosis.getValue();
+	Ti.API.info("dosis: " + dosis);
 }
